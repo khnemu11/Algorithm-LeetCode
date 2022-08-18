4f8103e451af2adba62a1adab8a26ef0b9c6874c
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2022-08-18 19:59:29 +0900

    Time: 518 ms (5.02%), Space: 67.4 MB (85.25%) - LeetHub

---
+++ 204-count-primes/204-count-primes.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int countPrimes(int n) {
+		boolean result[] = new boolean[n];
+		int answer = 0;
+		
+        if(n>1){
+            for(int i=2;i<n;i++) {
+			    result[i]= true;
+            }
+                result[0]=false;
+                result[1]=false;
+
+            for(int i=2;i*i<=n;i++) {
+                for(int j=i*2;j<n;j=j+i) {
+                    result[j]=false;
+                }
+            }
+            //System.out.println(Arrays.toString(result));
+            for(int i=2;i<n;i++) {
+                if(result[i]) {
+                    answer++;
+                }
+            }
+        }
+		
+		
+        
+        return answer;
+    }
+}
