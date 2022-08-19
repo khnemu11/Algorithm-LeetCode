5c0455629817b4e77439d5c46675763d39ca5e77
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2022-08-19 18:51:59 +0900

    Time: 5 ms (11.07%), Space: 42.1 MB (8.01%) - LeetHub

--- 342-power-of-four/342-power-of-four.java
+++ 342-power-of-four/342-power-of-four.java
@@ -1,9 +1,7 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
        if(n<0)  return false;
-		//System.out.println(Long.toBinaryString((long)n));
 		String binary = Long.toBinaryString((long)n);
-		//System.out.println(binary.replace("1", "").length() %2 == 0);
 		if(binary.charAt(0)=='1' 
 				&& binary.length()-binary.replace("1", "").length()==1
 				 &&  binary.replace("1", "").length() %2 == 0) {
