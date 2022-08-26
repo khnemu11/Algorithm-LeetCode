be5615a89ab6a1905f2c58d2c914806ba92801ee
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2022-08-26 19:44:56 +0900

    Time: 6 ms (81.07%), Space: 48.8 MB (28.37%) - LeetHub

--- 155-min-stack/155-min-stack.java
+++ 155-min-stack/155-min-stack.java
@@ -1,30 +1,24 @@
 class MinStack {
 	Stack<Integer> stack;
-	Stack<Integer> minStack;
+	int min;
 	
     public MinStack() {
         this.stack = new Stack<>();
-        this.minStack = new Stack<>();
+        this.min=Integer.MAX_VALUE;
     }
     
     public void push(int val) {
+    	if(val<=min) {
+    		stack.push(min);
+    		min=val;
+    	}
     	stack.push(val);
-    	if(minStack.isEmpty()) {
-    		minStack.push(val);
-    	}
-    	else {
-    		if(minStack.peek()>val) {
-    			minStack.push(val);
-    		}
-    		else{
-    			minStack.push(minStack.peek());
-    		}
-    	}
     }
     
     public void pop() {
-    	stack.pop();
-    	minStack.pop();
+    	if(stack.pop() ==min) {
+    		min=stack.pop();
+    	}
     }
     
     public int top() {
@@ -32,6 +26,6 @@ class MinStack {
     }
     
     public int getMin() {
-        return minStack.peek();
+        return min;
     }
 }
